0bec521370ee06536278a512255318b5b51cc23a
Scott McKenzie <scottymckenzie92@gmail.com>
2021-05-10 13:27:21 -0600

    Added preview image/gif. Setup example app.

--- app/src/main/java/com/scottymack/circleviewsample/MainActivity.kt
+++ app/src/main/java/com/scottymack/circleviewsample/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.scottymack.circleviewsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import com.larswerkman.lobsterpicker.OnColorListener
+import com.larswerkman.lobsterpicker.sliders.LobsterShadeSlider
+import com.scottymack.circleviewsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.seekBarBorderWidth.onProgressChanged { binding.circleView.borderWidth = it.toFloat() }
+        binding.seekBarShadowRadius.onProgressChanged { binding.circleView.shadowRadius = it.toFloat() }
+        binding.shadeSlider.onColorChanged {
+            binding.circleView.circleColor = it
+            binding.circleView.shadowColor = it
+        }
     }
+
+    //region Extensions
+    private fun SeekBar.onProgressChanged(onProgressChanged: (Int) -> Unit) {
+        setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                onProgressChanged(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Nothing
+            }
+        })
+    }
+
+    private fun LobsterShadeSlider.onColorChanged(onColorChanged: (Int) -> Unit) {
+        addOnColorListener(object : OnColorListener {
+            override fun onColorChanged(color: Int) {
+                onColorChanged(color)
+            }
+
+            override fun onColorSelected(color: Int) {
+                // Nothing
+            }
+        })
+    }
+    //endregion
+
 }
